6df61115636e63f781dcdce18f63d5242c364a88
Shrikant Pardhi <dynast.dev@gmail.com>
2022-05-28 07:37:55 +0530

    Objectmapper added

---
+++ src/main/java/com/dynast/examportal/util/ObjectMapperSingleton.java
@@ -0,0 +1,13 @@
+package com.dynast.examportal.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class ObjectMapperSingleton {
+    private static ObjectMapper INSTANCE;
+    public static ObjectMapper getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new ObjectMapper();
+        }
+        return INSTANCE;
+    }
+}
